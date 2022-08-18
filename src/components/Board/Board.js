b592b4c6f38f34f746d7608a4b62d70fc78f5b29
Taras Erechenko <ta300489@gmail.com>
2022-08-18 13:26:21 +0300

    fix id problem

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,7 +2,6 @@ import React from "react";
 import './Board.css';
 
 const Board = (props) => {
-        console.log(props)
     return (
         <div className="Board" >
             <div className="small-weather">
--- src/components/BottomBoard/BottomBoard.js
+++ src/components/BottomBoard/BottomBoard.js
@@ -8,11 +8,10 @@ const BottomBoard = ({ dataBoottomBoards,isLoaded} ) => {
     if (isLoaded) {
         let weather = [];
         dataBoottomBoards.map(data => weather.push(new DataWeather(data)))
-        console.log(weather)
         return (
             <div className="BottomBoard" >
                 {weather.map(data =>
-                    <Board icon={data.icon} temp={data.temp} date={data.date} key={data.dt} />
+                    <Board icon={data.icon} temp={data.temp} date={data.date} key={data.id} />
                 )}
             </div>
         )
